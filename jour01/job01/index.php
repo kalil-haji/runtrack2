2d041c869ac8db2a32af4425a4ddd80346df4548
kalil-haji <91876596+kalil-haji@users.noreply.github.com>
2021-11-02 12:01:46 +0100

    job01 variable

---
+++ jour01/job01/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Hello!</title>
+</head>
+<body>
+        
+<?php
+$str = 'LaPlateforme';
+print "$str";
+$str2 = 'Vive';
+$str3 = '!';
+print "$str2 $str $str3";
+
+
+$val = '6';
+print "$val";
+$val += '4';
+print "$val";
+
+$myBool = 'true';
+print "$myBool";
+$myBool = 'false';
+print "$myBool";
+
+?>
+
+</body>
+</html>
