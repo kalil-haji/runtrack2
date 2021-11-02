67bc0ce8b82ddd94fc7a6542aeb312a3a8cc8224
kalil-haji <91876596+kalil-haji@users.noreply.github.com>
2021-11-02 12:09:34 +0100

    job02 finis

---
+++ jour01/job02/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Hello!</title>
+</head>
+<body>
+        
+<?php
+/* Affichage via la variable str*/
+$str = 'Hello LaPlateforme!';
+print "$str</br>";
+
+
+/* Affichage via la fonction print*/
+print "Hello LaPlateforme!";
+
+
+
+?>
+
+</body>
+</html>
