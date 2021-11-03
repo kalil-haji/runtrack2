1f000fed84796eae94e923e74e365730476f31c2
kalil-haji <91876596+kalil-haji@users.noreply.github.com>
2021-11-03 13:26:40 +0100

    job final 5

---
+++ jour02/job05/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Hello!</title>
+</head>
+<body>
+        
+<?php
+
+$count = 0 ;
+$number = 2 ;
+while ($count < 168 )
+{
+$div_count=0;
+for ( $i=1;$i<=$number;$i++)
+{
+if (($number%$i)==0)
+{
+$div_count++;
+}
+}
+if ($div_count<3)
+{
+echo $number."</br>";
+$count=$count+1;
+}
+$number=$number+1;
+}
+
+?> 
+
+</body>
+</html>
