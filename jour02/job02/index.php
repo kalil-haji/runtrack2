ae0e13c55849c700e95c0dfd3fb6dadd62558e0d
kalil-haji <91876596+kalil-haji@users.noreply.github.com>
2021-11-03 13:25:34 +0100

    job 2

---
+++ jour02/job02/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Hello!</title>
+</head>
+<body>
+        
+<?php
+$var = 0;
+
+while($var <= 1337){
+if($var == 26 || $var == 37 || $var == 88 || $var == 1111){
+}
+
+else{
+    echo $var."</br>";
+    
+}
+$var += 1;
+}
+?>
+
+</body>
+</html>
